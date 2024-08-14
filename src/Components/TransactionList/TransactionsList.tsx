e131639a801d056c63a4d36a7001edaebe2d766c
dobliyal <rohit.dobliyal@binmile.com>
2024-08-14 15:24:38 +0530

    Rightswip

--- src/Components/TransactionList/TransactionsList.tsx
+++ src/Components/TransactionList/TransactionsList.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import React, { useRef } from "react";
+import { Animated, View, Text } from "react-native";
 import { Category, Transaction } from "../../Utils/Types/types";
 import TransactionListItem from "../TransactionListItem/TransactionListItem";
 import { styles } from "./styleTransactionList";
+import GestureRecognizer from 'react-native-swipe-gestures';
 
 const TransactionsList = ({
   transactions,
@@ -13,23 +14,55 @@ const TransactionsList = ({
   categories: Category[];
   deleteTransaction: (id: number) => Promise<void>;
 }) => {
+  const animationRefs = useRef<{ [key: number]: Animated.Value }>({});
+
+  transactions.forEach(transaction => {
+    if (!animationRefs.current[transaction.id]) {
+      animationRefs.current[transaction.id] = new Animated.Value(0);
+    }
+  });
+
+  const handleSwipeRight = (transactionId: number) => {
+    Animated.timing(animationRefs.current[transactionId], {
+      toValue: 300, 
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      deleteTransaction(transactionId);
+    });
+  };
+
   return (
     <View style={styles.container}>
       {transactions.map((transaction) => {
         const categoryForCurrentItem = categories.find(
           (category) => category.id === transaction.category_id
         );
+
         return (
-          <TouchableOpacity
+          <GestureRecognizer
             key={transaction.id}
-            activeOpacity={0.7}
-            onLongPress={() => deleteTransaction(transaction.id)}
+            onSwipeRight={() => handleSwipeRight(transaction.id)}
+            config={{
+              velocityThreshold: 0.3,
+              directionalOffsetThreshold: 80,
+            }}
           >
-            <TransactionListItem
-              transaction={transaction}
-              categoryInfo={categoryForCurrentItem}
-            />
-          </TouchableOpacity>
+            <Animated.View
+              style={{
+                transform: [
+                  {
+                    translateX: animationRefs.current[transaction.id],
+                  },
+                ],
+              }}
+            >
+              <TransactionListItem
+                transaction={transaction}
+                categoryInfo={categoryForCurrentItem}
+              />
+            </Animated.View>
+          </GestureRecognizer>
         );
       })}
     </View>
